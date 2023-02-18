5145d9296fcc5c344e0d8db47cd1691a77dd43d2
Daniel Alemán R <46462215+DanielAR27@users.noreply.github.com>
2023-02-17 22:56:21 -0600

    Creación del root para la ventana en Tkinter
    En este archivo, se abrirá la ventana principal de Tkinter y se pedirá al usuario el link del video de YouTube,

---
+++ YouTube Downloader/tkinter_root.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import tkinter as tk
+
+
+class Window():
+    def __init__(self):
+        # Se crea un objeto de tipo Tkinter, este será por así decirlo nuestro
+        # root en donde guardaremos todas las posibles configuraciones.
+        self.root = tk.Tk()
+        # Se le otorga una geometría en específico.
+        self.root.geometry('600x350')
+        # Se aplica este comando para que el tamaño no sea modificable.
+        self.root.resizable(0, 0)
+        # Se otorga un título a la ventana.
+        self.root.title('YouTube Video Downloader for Ubuntu')
+        # Se carga la foto de la aplicación.
+        app_logo = tk.PhotoImage(file='app_logo.png')
+        # Establecer la imagen de icono de la aplicación.
+        self.root.iconphoto(False, app_logo)
+        # Se agregan algunos textos dentro de la ventana.
+        tk.Label(self.root, text='YouTube Downloader',
+                 font='roboto 22 bold').pack()
+        tk.Label(self.root, text=('ADVERTENCIA: Desactive el antivirus '
+                                  'para que la descarga se de'
+                                  ' correctamente.'),
+                 font='roboto 9').place(x=50, y=157)
+        tk.Label(self.root, text='Inserte el enlace aquí: ',
+                 font='roboto 15').place(x=175, y=102)
+        # Se cree una ventana de texto en donde escribir el link del video.
+        link = tk.Entry(self.root, width=70,
+                        highlightthickness=3,
+                        highlightbackground='#ff0068',
+                        highlightcolor='#ff0068')
+        link.place(x=15, y=130)
+        # Se crea un botón para comenzar la descarga.
+        start_button = tk.Button(self.root, text='Comenzar', font='roboto 15',
+                                 bg='#ff0068', foreground='white', padx=2)
+        start_button.place(x=230, y=190)
+        # El root debe encontrarse en un loop para así correr el programa.
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    Window()
