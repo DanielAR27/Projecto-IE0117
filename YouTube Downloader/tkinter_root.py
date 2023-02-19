b80f274489994bc788012af08eff52d5de0ebb92
Daniel Alemán <copperdar27@gmail.com>
2023-02-18 18:35:07 -0600

    Se agregan cambios dentro del root en Tkinter.
    Se importan el archivo 'download_functions' como 'df' y al botón de inicio se le agrega la funcionalidad de poder utilizar la función "start_download".

--- YouTube Downloader/tkinter_root.py
+++ YouTube Downloader/tkinter_root.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+# LIBRERIAS UTILIZADAS
 import tkinter as tk
+import download_functions as df
 
 
 class Window():
@@ -7,6 +10,9 @@ class Window():
         # Se crea un objeto de tipo Tkinter, este será por así decirlo nuestro
         # root en donde guardaremos todas las posibles configuraciones.
         self.root = tk.Tk()
+        # Se crea un objeto de tipo Download Functions para posteriormente
+        # utilizar algunas funciones útiles dentro del root.
+        utilities = df.Functions(self.root)
         # Se le otorga una geometría en específico.
         self.root.geometry('600x350')
         # Se aplica este comando para que el tamaño no sea modificable.
@@ -34,11 +40,9 @@ class Window():
         link.place(x=15, y=130)
         # Se crea un botón para comenzar la descarga.
         start_button = tk.Button(self.root, text='Comenzar', font='roboto 15',
-                                 bg='#ff0068', foreground='white', padx=2)
+                                 bg='#ff0068', foreground='white', padx=2,
+                                 command=lambda:
+                                 utilities.start_download(link))
         start_button.place(x=230, y=190)
         # El root debe encontrarse en un loop para así correr el programa.
         self.root.mainloop()
-
-
-if __name__ == "__main__":
-    Window()
